3be5976879d5c0c871000ba75ffc7cc40c5af32f
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2022-02-12 11:50:01 -0500

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Favorite color of various people data
+#'
+#' @description \code{favcolordata} returns a dataframe of the favorite color of various people.
+#'
+#' @return Returns a dataframe of the favorite color of various people.
+#'
+#' @docType data
+#'
+#' @usage data(color_data)
+#'
+#' @format An dataframe of individual-level observations with the following variables:
+#'
+#' \describe{
+#' \item{\code{name}}{A character vector of individual names.}
+#' \item{\code{fav_color}}{A character vector of one of: black, white, rainbow.}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source \url{https://www.tellingstorieswithdata.com/storing-and-retrieving-data.html}
+#'
+"color_data"
